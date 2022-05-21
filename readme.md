76a9e0896c76df2f2bd48c4d9a33dc2d19329e8c
Alberto F. Chávez Pérez <andalusier@pm.me>
2022-05-21 18:14:28 +0200

    Instructions

--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
-Logga in på https://fp.trafikverket.se/Boka/#/
+# Find time for driving test
 
+Request slots, find slot, send push notification. Can be changed to Telegram.
 
+## Instructions
+1. Log in to https://fp.trafikverket.se/Boka/#/
+2. Make changes to the secrets file
+3. > node trafikverket.js
--- secrets.js
+++ secrets.js
@@ -1,13 +1,11 @@
+// Do not commit this file
+
 module.exports = {
     pushOverUser: "x",
     pushOverToken: "x",
-    // network - occasion bundles - header - paste string
+    // network --> occasion bundles --> header --> paste string
     headerCookiesFromMadeSearach: "xxx",
-    //network - occasion bundles - payload - paste object
+    // network --> occasion bundles --> payload --> paste object
     requestBodyFromMadeSearch: {"bookingSession":{"socialSecurityNumber":"xxxxxxxx-xxxx","licenceId":5,"bookingModeId":0,"ignoreDebt":false,"ignoreBookingHindrance":false,"examinationTypeId":0,"excludeExaminationCategories":[],"rescheduleTypeId":0,"paymentIsActive":false,"paymentReference":null,"paymentUrl":null,"searchedMonths":0},"occasionBundleQuery":{"startDate":"1970-01-01T00:00:00.000Z","searchedMonths":0,"locationId":1000019,"nearbyLocationIds":[],"vehicleTypeId":2,"tachographTypeId":1,"occasionChoiceId":1,"examinationTypeId":12}
     },
 }
-
-
-
-
